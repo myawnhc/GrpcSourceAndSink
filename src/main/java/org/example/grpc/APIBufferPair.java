8aae5542e99016ccb748b1a731db0dea5b9469c6
myawnhc <mike.yawn@hazelcast.com>
2023-05-22 14:00:50 -0400

    Update copyright, advance Hazelcast Platform version to 5.3

--- src/main/java/org/example/grpc/APIBufferPair.java
+++ src/main/java/org/example/grpc/APIBufferPair.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright (c) Hazelcast, Inc. 2022-2023.
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *
+ *
+ */
 package org.example.grpc;
 
 import com.hazelcast.collection.IQueue;
@@ -11,7 +29,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
--- startService.sh
+++ startService.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#
+# /*
+#  * Copyright (c) Hazelcast, Inc. 2022-2023.
+#  *
+#  *  Licensed under the Apache License, Version 2.0 (the "License");
+#  *  you may not use this file except in compliance with the License.
+#  *  You may obtain a copy of the License at
+#  *
+#  *    http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  *  Unless required by applicable law or agreed to in writing, software
+#  *  distributed under the License is distributed on an "AS IS" BASIS,
+#  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  *  See the License for the specific language governing permissions and
+#  *  limitations under the License.
+#  */
+#
+#
+
 # TODO: Need 'with dependencies' versions of these if we're going to run from script rather than IDE
 #   Actually if we do Tests with dependencies that should be all we need since it will pull in the connector
 export CONNECTOR_JAR=target/GrpcSourceAndSink-1.0-SNAPSHOT.jar
